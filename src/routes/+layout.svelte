d2ece37391ac89ee3e0b3f9af24e10d390eca069
ad4m0wx <solal.adamowicz@gmail.com>
2026-04-17 17:08:37 +0200

    fix ?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link rel="manifest" href="{base}/manifest.webmanifest" />
+	<link rel="apple-touch-icon" href="{base}/icon.svg" />
+</svelte:head>
 {@render children()}
